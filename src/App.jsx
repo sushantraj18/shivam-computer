f8f61780dd9f3209a71c4b78d13c48e6404f7cc6
sushantraj18 <codexsushant@gmail.com>
2025-03-02 00:11:31 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+
 
 function App() {
   return (
-    <div>App</div>
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' Component={Home}/>
+      <Route path='/about' Component={About}/>
+      <Route path='/contact-us' Component={Contact}/>
+    </Routes>
+    </BrowserRouter>
+  
   )
 }
 
